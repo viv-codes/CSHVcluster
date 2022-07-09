26992bab7d3b3cbb92be3055633c267dbff22c47
viv-codes <vhafener@outlook.com>
2022-07-09 13:24:52 -0400

    Wrote second part of install

--- README.md
+++ README.md
@@ -5,8 +5,9 @@
 2. apt update && apt install vim
 3. make sure swap is disabled in fstab: `vim /etc/fstab`
 4. is swap on? `cat /prox/meminfo | grep "Swap"` if so, reboot and check again to ensure it's off.
-5. `chmod +x install.sh` and `./install.sh`
-6. reboot
+5. `curl https://raw.githubusercontent.com/viv-codes/CSHVcluster/main/install.sh > install.sh`
+6. `chmod +x install.sh` and `./install.sh`
+7. reboot
  
 
  
---
+++ install2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+curl -sSL https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/Debian_11/Release.key | apt-key add -
+curl -sSL https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/1.22/Debian_11/Release.key | apt-key add -
+curl -sSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/Debian_11/ /" > /etc/apt/sources.list.d/libcontainers.list
+echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/1.22/Debian_11/ /" > /etc/apt/sources.list.d/crio.list
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main"  > /etc/apt/sources.list.d/kubernetes.list
+apt update -y && apt install -y cri-o cri-o-runc
+systemctl enable --now crio
+apt install "kubelet=1.24.2-00" "kubeadm=1.24.2-00" "kubectl=1.24.2-00"
+apt-mark hold kubelet && apt-mark hold kubeadm && apt-mark hold kubectl
